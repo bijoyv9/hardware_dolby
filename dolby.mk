bd474d70a3b38981609cc2c32c81280a98c0fab8
ryukftw <workforbijoy9@gmail.com>
2025-09-27 17:05:27 +0000

    dolby: Build Oplus Dolby

--- dolby.mk
+++ dolby.mk
@@ -101,6 +101,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     DSPVolumeSynchronizer
 
+# OPlus Dolby
+PRODUCT_PACKAGES += \
+    OplusDolby
+
 # Dolby Proprietary blobs
 PRODUCT_COPY_FILES += \
     $(DOLBY_PATH)/proprietary/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.hardware.dms@2.0-service.rc \
